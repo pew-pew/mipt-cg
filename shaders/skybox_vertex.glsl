89f59ae2965adbacdb82466b844f0ccf673dfe4c
Roman <roma_klimovitskiy@mail.ru>
2021-05-24 21:14:07 +0300

    Added skybox

---
+++ shaders/skybox_vertex.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+layout (location = 0) in vec3 pos_model;
+
+out vec3 tex_coord;
+
+uniform mat4 V, P;
+
+void main()
+{
+  tex_coord = pos_model;
+  gl_Position = P * mat4(mat3(V)) * vec4(pos_model, 1.0);
+}
